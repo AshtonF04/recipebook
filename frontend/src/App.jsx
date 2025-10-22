0398b107f1f19a2a9f97ce1341e1d4ce207ac25d
Ashton Franklin <ashtonfranklin772@gmail.com>
2025-10-22 18:01:12 -0400

    Created pages setup for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
+// Import external packages
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Import CSS Files
 import './App.css'
+
+// Pages and Components
+import Home from './pages/home'
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>React App</h1>
+    <BrowserRouter>
+      {/* Navigation Bar */}
+      
+      {/* Pages Div */}
+      <div className="pages">
+        <Routes>
+          {/* Home Page */}
+          <Route
+            path='/'
+            element={<Home />}
+          />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   )
 }
 
